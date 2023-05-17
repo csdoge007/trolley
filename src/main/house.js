06a0f0b32ae8a0c3584e2d447c01fd1ad28f92b6
csdoge007 <1765796778@qq.com>
2023-05-17 22:25:37 +0800

    third

--- src/main/house.js
+++ src/main/house.js
@@ -1,23 +1,33 @@
-
 import * as THREE from 'three';
 import { PerspectiveCamera } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-let scene, renderer, camera
+import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
+let scene, renderer, camera, labelRenderer
 let buildingPositions = []
 const SCWIDTH = 1000, SCHEIGHT = 1000, blockWidth = 50, blockHeight = 50
+const mp = new Array(20).fill(0).map(() => new Array(20).fill(false)) // false表示非道路
 main()
 function main() {
     scene = createScene()
     renderer = createRenderer()
     camera = createCamera()
     const light = createLight()
-    const control = createControl()
     const ground = createGround()
     const stBuilding = createStBuilding()
     createRoad()
     createBuildings(stBuilding)
+    initLabelRenderer()
+    const control = createControl()
+    setTrolley()
     requestAnimationFrame(render)
+}
+// 初始化标签渲染器
+function initLabelRenderer() {
+    labelRenderer = new CSS2DRenderer();
+    labelRenderer.setSize(window.innerWidth - 201, window.innerHeight);
+    labelRenderer.domElement.style.position = "absolute";
+    labelRenderer.domElement.style.top = "0px";
+    document.getElementById("c").appendChild(labelRenderer.domElement);
 }
 // 创建场景
 function createScene() {
@@ -100,8 +110,27 @@ function createBuildings(cube) {
         [8, 5],
         [2, 7],
         [3, 8],
-        [6, 5]  // 交点
+        [6, 5],
+        [1, 1],
+        [10, 6],
+        [16, 2],
+        [16, 4],
+        [18, 2],
+        [11, 8],
+        [15, 8],
+        [8, 11],
+        [8, 13],
+        [11, 13],
+        [13, 11],
+        [14, 13],
+        [14, 16],
+        [13, 18],
+        [18, 10],
+        [18, 14],
+        [18, 16]
     ]
+    let c = document.getElementById('c')
+    c.appendChild(renderer.domElement)
     for (let i = 0; i < building_positions.length; i++) {
         let cloneCube = cube.clone()
         let worldPosition = getWorldPosition(building_positions[i][0], building_positions[i][1])
@@ -142,6 +171,18 @@ function createBuildings(cube) {
             fragmentShader: fragmentShaderReplacements
         })
         cloneCube.material = cubeMaterial
+
+        let mark = document.createElement('div')
+        mark.className = "building"
+        c.appendChild(mark)
+        let name = document.createElement('div')
+        name.className = "name"
+        name.innerHTML = i + 1 + "号房子"
+        mark.appendChild(name)
+        const marktag = new CSS2DObject(mark)
+        marktag.position.set(x, height + 10, z)
+        marktag.scale.set(0.5, 0.5, 0.5)
+        scene.add(marktag)
         scene.add(cloneCube)
     }
 }
@@ -152,19 +193,9 @@ function getWorldPosition(x, z) {
         z: (-SCHEIGHT / 2) + z * blockHeight
     }
 }
-// 创建房子
-function createHouse() {
-    const house = new THREE.Group()
-    const sideWall = createSideWall(house)
-    const sideWall2 = createSideWall(house)
-    sideWall2.position.z = 30;
-    createFrontWall(house)
-    scene.add(house)
-    return house
-}
 // 创建照相机控制器
 function createControl() {
-    let control = new OrbitControls(camera, renderer.domElement);
+    let control = new OrbitControls(camera, labelRenderer.domElement);
     control.enableRotate = true; //启用旋转
     control.enablePan = true; //启用平移
     control.enableZoom = true;//启用缩放
@@ -173,90 +204,27 @@ function createControl() {
 // 渲染函数
 function render() {
     renderer.render(scene, camera)
+    labelRenderer.render(scene, camera)
     requestAnimationFrame(render)
-}
-// 创建侧墙
-function createSideWall(house) {
-    // const shape = new THREE.Shape();
-    // shape.moveTo(-10, 0);
-    // shape.lineTo(10, 0);
-    // shape.lineTo(10, 10);
-    // shape.lineTo(0, 15);
-    // shape.lineTo(-10, 10);
-    // shape.lineTo(-10, 0);
-    // const extrudeGeometry = new THREE.ExtrudeGeometry(shape);
-    const heartShape = new THREE.Shape();
-
-    heartShape.moveTo(25, 25);
-    heartShape.bezierCurveTo(25, 25, 20, 0, 0, 0);
-    heartShape.bezierCurveTo(- 30, 0, - 30, 35, - 30, 35);
-    heartShape.bezierCurveTo(- 30, 55, - 10, 77, 25, 95);
-    heartShape.bezierCurveTo(60, 77, 80, 55, 80, 35);
-    heartShape.bezierCurveTo(80, 35, 80, 0, 50, 0);
-    heartShape.bezierCurveTo(35, 0, 25, 25, 25, 25);
-
-    const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-    const extrudeGeometry = new THREE.ExtrudeGeometry(heartShape, extrudeSettings);
-
-    const loader = new THREE.TextureLoader()
-    const sideWall = new THREE.Mesh(extrudeGeometry, new THREE.MeshBasicMaterial());
-    loader.load('./img/wall.jpg', function (texture) {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set(0.01, 0.005);
-        sideWall.material.map = texture;
-        house.add(sideWall);
-    })
-    return sideWall;
-}
-// 创建前墙
-function createFrontWall(house) {
-    const shape = new THREE.Shape();
-    shape.moveTo(-15, 0);
-    shape.lineTo(15, 0);
-    shape.lineTo(15, 10);
-    shape.lineTo(-15, 10);
-    shape.lineTo(-15, 0);
-
-    const window = new THREE.Path();
-    window.moveTo(3, 3)
-    window.lineTo(8, 3)
-    window.lineTo(8, 8)
-    window.lineTo(3, 8);
-    window.lineTo(3, 3);
-    shape.holes.push(window);
-
-    const door = new THREE.Path();
-    door.moveTo(-3, 0)
-    door.lineTo(-3, 8)
-    door.lineTo(-8, 8)
-    door.lineTo(-8, 0);
-    door.lineTo(-3, 0);
-    shape.holes.push(door);
-
-    const extrudeGeometry = new THREE.ExtrudeGeometry(shape)
-
-    const loader = new THREE.TextureLoader()
-    loader.load('./img/wall.jpg', function (texture) {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set(0.01, 0.005);
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-        const frontWall = new THREE.Mesh(extrudeGeometry, material);
-        frontWall.position.z = 15;
-        frontWall.position.x = 10;
-        frontWall.rotation.y = Math.PI * 0.5;
-        house.add(frontWall);
-    });
 }
 // 创建道路贴图
 function createRoad() {
+    setRoad()
     createTexture('./img/road.png', (texture) => {
-        for (let i = 0; i < 5; i++) {
-            const textureGeometry = new THREE.PlaneGeometry(blockWidth, blockHeight)
-            const textureMesh = new THREE.Mesh(textureGeometry, new THREE.MeshBasicMaterial({ map: texture }))
-            textureMesh.rotation.x = -0.5 * Math.PI;
-            textureMesh.receiveShadow = true
-            textureMesh.position.set(i * 50, 1,0)
-            scene.add(textureMesh)
+        for (let i = 1; i < 20; i++) {
+            for (let j = 1; j < 20; j++) {
+                if (mp[i][j]) {
+                    let worldPosition = getWorldPosition(i, j)
+                    let x = worldPosition.x
+                    let z = worldPosition.z
+                    const textureGeometry = new THREE.PlaneGeometry(blockWidth, blockHeight)
+                    const textureMesh = new THREE.Mesh(textureGeometry, new THREE.MeshBasicMaterial({ map: texture }))
+                    textureMesh.rotation.x = -0.5 * Math.PI;
+                    textureMesh.receiveShadow = true
+                    textureMesh.position.set(x, 5, z)
+                    scene.add(textureMesh)
+                }
+            }
         }
     })
 }
@@ -269,4 +237,63 @@ function createTexture(url, func) {
         texture.repeat.set(100, 100);
         func(texture)
     })
+}
+// 规划道路
+function setRoad() {
+    for (let i = 2; i <= 7; i++) mp[i][1] = true
+    for (let i = 2; i <= 6; i++) mp[1][i] = true
+    for (let i = 2; i <= 6; i++) mp[7][i] = true
+    for (let i = 2; i <= 4; i++) mp[i][6] = true
+    for (let i = 7; i <= 9; i++) mp[4][i] = true
+    for (let i = 5; i <= 16; i++) mp[i][7] = true
+    for (let i = 1; i <= 19; i++) mp[17][i] = true
+    for (let i = 5; i <= 9; i++) mp[i][9] = true
+    for (let i = 10; i <= 19; i++) mp[9][i] = true
+    for (let i = 10; i <= 16; i++) mp[i][19] = true
+    for (let i = 10; i <= 18; i++) {
+        mp[12][i] = true
+        mp[15][i] = true
+    }
+    for (let i = 13; i <= 14; i++) {
+        mp[i][10] = true
+    }
+}
+// 绘制小车
+function createTrolley() {
+    const car = new THREE.Group()
+    const sideLength = 8
+    const bodyGeometry = new THREE.BoxGeometry(sideLength, sideLength, sideLength); // 绘制立方体
+    const bodyMaterial = new THREE.MeshPhongMaterial({ color: 0x6688aa })
+    const bodyMesh = new THREE.Mesh(bodyGeometry, bodyMaterial)
+    bodyMesh.castShadow = true // 允许投射阴影
+    bodyMesh.position.y = sideLength / 2
+    car.add(bodyMesh)
+    const wheelRadius = 3 // 轮胎圆柱体的半径
+    const wheelThickness = 1.8 // 轮胎的厚度
+    const wheelSegments = 20  // 分划
+    const wheelGeometry = new THREE.CylinderGeometry(
+        wheelRadius,
+        wheelRadius,
+        wheelThickness,
+        wheelSegments
+    )
+    const wheelMaterial = new THREE.MeshPhongMaterial({
+        color: 0x888888
+    })
+    const cx = sideLength / 2 + wheelThickness / 2   // 设置两个轮胎所处的位置
+    const wheelMeshes = [-cx, cx].map(x => {
+        const mesh = new THREE.Mesh(wheelGeometry, wheelMaterial)
+        mesh.rotation.z = Math.PI * 0.5     // 将轮胎立起来
+        mesh.position.set(x, wheelRadius, 0)
+        mesh.castShadow = true
+        car.add(mesh)
+        return mesh
+    })
+    scene.add(car)
+    return car
+}
+// 设置小车
+function setTrolley() {
+    const trolley = createTrolley()
+    trolley.position.set(getWorldPosition(9,11).x,5,getWorldPosition(9,11).z)
 }
