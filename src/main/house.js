95c0f43e39d7f6e315a3b44d2e695f43c4b40d02
csdoge007 <1765796778@qq.com>
2023-05-19 21:49:47 +0800

    four

--- src/main/house.js
+++ src/main/house.js
@@ -1,11 +1,24 @@
+import * as dat from "dat.gui"
 import * as THREE from 'three';
-import { PerspectiveCamera } from 'three';
+import { MeshToonMaterial, PerspectiveCamera, Vector2 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
 let scene, renderer, camera, labelRenderer
 let buildingPositions = []
 const SCWIDTH = 1000, SCHEIGHT = 1000, blockWidth = 50, blockHeight = 50
 const mp = new Array(20).fill(0).map(() => new Array(20).fill(false)) // false表示非道路
+let building_positions
+let trolley, wheelMeshes
+let statusText,controllerText
+const carPosition = new Vector2() // 小车坐标
+let troPosition,endPosition // 小车位置
+let arrowMesh // 表示箭头
+let nowCurve // 表示目前道路的曲线
+let ismove = false;// 表示小车目前是否在移动
+let lasttime = 0     // 表示上一次刷新时候的时间
+let movetime = 0     // 此次任务的时间
+let vmove = 0        // 移动的速度   
+let enCamera,troCamera
 main()
 function main() {
     scene = createScene()
@@ -19,6 +32,7 @@ function main() {
     initLabelRenderer()
     const control = createControl()
     setTrolley()
+    createGui()
     requestAnimationFrame(render)
 }
 // 初始化标签渲染器
@@ -46,10 +60,10 @@ function createRenderer() {
 }
 // 创建相机
 function createCamera() {
-    const camera = makeCamera()
-    camera.position.set(SCWIDTH, SCWIDTH, SCWIDTH)
-    camera.lookAt(0, 0, 0)
-    return camera
+    enCamera = makeCamera()
+    enCamera.position.set(SCWIDTH, SCWIDTH, SCWIDTH)
+    enCamera.lookAt(0, 0, 0)
+    return enCamera
 }
 // 设置相机
 function makeCamera(fov = 40) {
@@ -104,7 +118,7 @@ function createStBuilding() {
 }
 // 创建建筑群落
 function createBuildings(cube) {
-    const building_positions = [
+    building_positions = [
         [6, 2],
         [5, 7],
         [8, 5],
@@ -202,7 +216,37 @@ function createControl() {
     return control
 }
 // 渲染函数
-function render() {
+function render(time) {
+    time *= 0.001
+    if (!ismove) {
+        if (arrowMesh) {
+            scene.remove(arrowMesh)
+            arrowMesh.geometry.dispose()
+            arrowMesh.material.dispose()
+        }
+    } else {
+        movetime += (time - lasttime)
+        let len = nowCurve.getLength()
+        if (movetime * vmove < len) {
+            nowCurve.getPointAt(movetime * vmove / len, carPosition)
+            let frontp = new Vector2()
+            nowCurve.getPointAt(Math.min(1.0,movetime * vmove * 2 / len),frontp)
+            // 设置小车朝向
+            trolley.lookAt(carPosition.x, 5, carPosition.y)
+            // 设置小车位置
+            trolley.position.set(carPosition.x, 5, carPosition.y)
+            // 车轱辘的转动
+            wheelMeshes.forEach(obj => {
+                obj.rotation.x = time * 3
+            })
+        } else {
+            ismove = false
+            statusText.setValue('小车空闲中')
+            troPosition = endPosition
+            controllerText.setValue(troPosition + '号房子')
+        }
+    }
+    lasttime = time
     renderer.render(scene, camera)
     labelRenderer.render(scene, camera)
     requestAnimationFrame(render)
@@ -245,7 +289,7 @@ function setRoad() {
     for (let i = 2; i <= 6; i++) mp[7][i] = true
     for (let i = 2; i <= 4; i++) mp[i][6] = true
     for (let i = 7; i <= 9; i++) mp[4][i] = true
-    for (let i = 5; i <= 16; i++) mp[i][7] = true
+    for (let i = 6; i <= 16; i++) mp[i][7] = true
     for (let i = 1; i <= 19; i++) mp[17][i] = true
     for (let i = 5; i <= 9; i++) mp[i][9] = true
     for (let i = 10; i <= 19; i++) mp[9][i] = true
@@ -281,7 +325,7 @@ function createTrolley() {
         color: 0x888888
     })
     const cx = sideLength / 2 + wheelThickness / 2   // 设置两个轮胎所处的位置
-    const wheelMeshes = [-cx, cx].map(x => {
+    wheelMeshes = [-cx, cx].map(x => {
         const mesh = new THREE.Mesh(wheelGeometry, wheelMaterial)
         mesh.rotation.z = Math.PI * 0.5     // 将轮胎立起来
         mesh.position.set(x, wheelRadius, 0)
@@ -289,11 +333,189 @@ function createTrolley() {
         car.add(mesh)
         return mesh
     })
+    troCamera = makeCamera()
+    car.add(troCamera)
+    troCamera.position.y = 20
+    troCamera.position.z = -10
+    troCamera.lookAt(0,20,0)
     scene.add(car)
     return car
 }
 // 设置小车
 function setTrolley() {
-    const trolley = createTrolley()
-    trolley.position.set(getWorldPosition(9,11).x,5,getWorldPosition(9,11).z)
+    trolley = createTrolley()
+    // ismove = true
+    // vmove = 40
+    troPosition = 14 // 小车初始位于14号房子
+    let stxy = getWorldPosition(9, 11)
+    trolley.position.set(stxy.x, 5, stxy.z)
+    carPosition.x = stxy.x
+    carPosition.y = stxy.z
+}
+// bfs， 从(dx,dy) -> (tx,ty)
+function bfs(dx, dy, tx, ty) {
+    if (!mp[dx][dy] || !mp[tx][ty]) return null
+    let q = []
+    let pre = new Array(25).fill(0).map(() => new Array(25).fill(0))
+    let vis = new Array(25).fill(0).map(() => new Array(25).fill(false))
+    let road = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    vis[dx][dy] = true
+    q.push({ x: dx, y: dy, step: 0 })
+    while (q.length > 0) {
+        let t = q.shift()
+        if (t.x == tx && t.y == ty) break
+        for (let i = 0; i < 4; i++) {
+            let kx = t.x + road[i][0]
+            let ky = t.y + road[i][1]
+            if (mp[kx][ky] && !vis[kx][ky]) {
+                vis[kx][ky] = true
+                q.push({ x: kx, y: ky, step: t.step + 1 })
+                pre[kx][ky] = { x: t.x, y: t.y }
+            }
+        }
+    }
+    let resQueue = []
+    let p = { x: tx, y: ty }
+    while (p != 0) {
+        resQueue.push(p)
+        p = pre[p.x][p.y]
+    }
+    let points = []
+    while (resQueue.length > 0) {
+        let resPoint = resQueue.pop()
+        let worldPosition = getWorldPosition(resPoint.x, resPoint.y)
+        let ax = worldPosition.x
+        let az = worldPosition.z
+        points.push(new THREE.Vector2(ax, az))
+    }
+    const curve = new THREE.SplineCurve(points)
+    getArrowLine(curve)
+    ismove = true
+    movetime = 0
+    nowCurve = curve
+    statusText.setValue('小车正忙')
+}
+// 绘制箭头
+function getArrowLine(curve) {
+    const vertexShader = `
+    attribute vec3 position;
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;
+    void main() {
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    }
+    `;
+    const fragmentShader = `
+    void main() {
+      gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+    }
+`;
+    const numPoints = Math.floor(curve.getLength() / 15)
+    const points = curve.getPoints(numPoints)  // 生成点集
+
+    const geometry = new THREE.BufferGeometry();
+    const vertices = [];
+    for (let i = 1; i < points.length; i++) {
+        let directionVector = new THREE.Vector2(points[i].x - points[i - 1].x, points[i].y - points[i - 1].y).normalize()
+        let dx = directionVector.x * 5
+        let dy = directionVector.y * 5
+        let x0 = points[i].x
+        let y0 = points[i].y
+        let x1 = x0 - dx
+        let y1 = y0 - dy
+        let v1 = new THREE.Vector3(dx, 0, dy)
+        let v2 = new THREE.Vector3(0, 1, 0)
+        let perpendicular = new THREE.Vector3().crossVectors(v1, v2).normalize()
+        let x3 = x1 - dx + perpendicular.x * 5
+        let y3 = y1 - dy + perpendicular.z * 5
+        let x2 = x3 - perpendicular.x * 2 * 5
+        let y2 = y3 - perpendicular.z * 2 * 5
+        vertices.push(x0, 0.0, y0)
+        vertices.push(x2, 0.0, y2)
+        vertices.push(x1, 0.0, y1)
+        vertices.push(x0, 0.0, y0)
+        vertices.push(x1, 0.0, y1)
+        vertices.push(x3, 0.0, y3)
+    }
+    // itemSize = 3 因为每个顶点都是一个三元组。
+    geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
+    const material = new THREE.RawShaderMaterial({
+        vertexShader,
+        fragmentShader
+    })
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.y = 10
+    arrowMesh = mesh
+    scene.add(mesh)
+}
+// gui面板设计
+function createGui() {
+    const gui = new dat.GUI()
+    let target = -1
+    const obj = {
+        Position: 14,
+        text: troPosition + '号房子',
+        status: '小车空闲中',
+        speed: 1,
+        camera: '高空相机',
+        onClickButton: function () {
+            if (target != -1) {
+                if (this.status == '小车正忙' || target == troPosition || this.speed == 0 || target == -1) return
+                else {
+                    let d = getRoadPosition(
+                        building_positions[troPosition - 1][0],
+                        building_positions[troPosition - 1][1]
+                    )
+                    let t = getRoadPosition(
+                        building_positions[target - 1][0],
+                        building_positions[target - 1][1]
+                    )
+                    bfs(d.x,d.y,t.x,t.y)
+                    endPosition = target
+                }
+            } else alert('请重新选择目的地')
+        }
+    };
+    controllerText = gui.add(obj, 'text').name('小车所在位置')
+    statusText = gui.add(obj, 'status').name('小车的状态')
+    let buildings = []
+    for (let i = 0; i < building_positions.length; i++) {
+        buildings.push(i + 1 + '号房子')
+    }
+    let speeds = []
+    for (let i = 1; i <= 10; i++) {
+        speeds.push(i * 5 + 'm/s')
+    }
+    gui.add(obj, 'Position', buildings).name('终点').onChange(function (value) {
+        let index = value.indexOf('号')
+        let num = parseInt(value.slice(0, index))
+        target = num
+    });
+    gui.add(obj, 'speed', speeds).name('小车速度').onChange(function (value) {
+        if(obj.status == '小车正忙') {
+            alert('小车正在行驶，请不要随意更改速度')
+            return 
+        }
+        let index = value.indexOf('m')
+        let num = parseInt(value.slice(0, index))
+        vmove = num
+    })
+    gui.add(obj,'camera',['高空视角','小车视角']).name('视角切换').onChange(function (value) {
+        if(value == '小车视角') {
+            camera = troCamera
+        } else camera = enCamera
+    })
+    gui.add(obj, 'onClickButton').name('\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0开始配送');
+}
+// 获取该坐标附近最近的道路坐标
+function getRoadPosition(x, y) {
+    let road = [0, -1, 0, 1]
+    for (let i = 0; i < 4; i++) {
+        let dx = road[i] + x
+        let dy = road[(i + 1) % 4] + y
+        if (dx >= 1 && dx <= 19 && dy >= 1 && dy <= 19 && mp[dx][dy]) {
+            return {x: dx,y: dy}
+        }
+    }
 }
